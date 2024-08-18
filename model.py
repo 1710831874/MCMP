21e1f679b63a471b1135f1e3a51f2f3f633357db
ikun-Wu <66245856+WuIkun5658@users.noreply.github.com>
2024-08-18 15:53:30 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,314 @@
+import torch
+import torch.nn as nn
+
+# 多头注意力
+class MultiHeaded_NonLocalBlock(nn.Module):
+    def __init__(self, in_channels, inter_channels=None, dimension=3, sub_sample=True, bn_layer=True, n_heads=8):
+        super(MultiHeaded_NonLocalBlock, self).__init__()
+
+        assert dimension in [1, 2, 3]
+
+        self.dimension = dimension
+        self.sub_sample = sub_sample
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+        if dimension == 3:
+            conv_nd = nn.Conv3d
+            max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
+            bn = nn.BatchNorm3d
+        elif dimension == 2:
+            conv_nd = nn.Conv2d
+            max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
+            bn = nn.BatchNorm2d
+        else:
+            conv_nd = nn.Conv1d
+            max_pool_layer = nn.MaxPool1d(kernel_size=(2))
+            bn = nn.BatchNorm1d
+
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                         kernel_size=1, stride=1, padding=0)
+
+        if bn_layer:
+            # V
+            self.W = nn.Sequential(
+                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                        kernel_size=1, stride=1, padding=0),
+                bn(self.in_channels)
+            )
+            nn.init.constant_(self.W[1].weight, 0)
+            nn.init.constant_(self.W[1].bias, 0)
+        else:
+            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                             kernel_size=1, stride=1, padding=0)
+            nn.init.constant_(self.W.weight, 0)
+            nn.init.constant_(self.W.bias, 0)
+
+        # Q
+        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+        # K
+        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+        if sub_sample:
+            self.g = nn.Sequential(self.g, max_pool_layer)
+            self.phi = nn.Sequential(self.phi, max_pool_layer)
+
+    def forward(self, x, return_nl_map=False):
+        """
+        :param x: (b, c, t, h, w)
+        :param return_nl_map: if True return z, nl_map, else only return z.
+        :return:
+        """
+
+        batch_size = x.size(0)
+
+        # 输入，维度变换
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)  # (bs,1,256)
+
+        # Q
+        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+        theta_x = theta_x.permute(0, 2, 1)  # (bs,1,256)
+
+        # K
+        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)  # (bs,256，1)
+
+        f = torch.matmul(theta_x, phi_x)  # (bs,1,1) 计算相似度矩阵，形状为`(批次大小，空间维度的乘积，空间维度的乘积)`
+        N = f.size(-1)  # 1 获取相似度矩阵的形状。
+        f_div_C = f / N  # (bs,1,1)
+
+        y = torch.matmul(f_div_C, g_x)  # (bs,1,256)  将 f_div_C 矩阵和 g_x 矩阵相乘，得到 y 矩阵，形状为 (batch_size, t*h*w, inter_channels)
+        y = y.permute(0, 2, 1).contiguous()  # (bs,256，1)
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])  # (bs,256，1)
+
+        # V
+        W_y = self.W(y)  # (bs,512,1)
+
+        z = W_y + x  # (bs,512,1)
+
+        if return_nl_map:
+            return z, f_div_C
+        return z
+
+class MultiHeaded_NONLocalBlock1D(MultiHeaded_NonLocalBlock):
+    def __init__(self, in_channels, inter_channels=None, sub_sample=True, bn_layer=True, n_heads=8):
+        super(MultiHeaded_NONLocalBlock1D, self).__init__(in_channels,
+                                              inter_channels=inter_channels,
+                                              dimension=1, sub_sample=sub_sample,
+                                              bn_layer=bn_layer, n_heads=n_heads)
+
+
+
+# 单头注意力
+class _NonLocalBlockND(nn.Module):
+    def __init__(self, in_channels, inter_channels=None, dimension=3, sub_sample=True, bn_layer=True):
+        super(_NonLocalBlockND, self).__init__()
+
+        assert dimension in [1, 2, 3]
+
+        self.dimension = dimension
+        self.sub_sample = sub_sample
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+        if dimension == 3:
+            conv_nd = nn.Conv3d
+            max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
+            bn = nn.BatchNorm3d
+        elif dimension == 2:
+            conv_nd = nn.Conv2d
+            max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
+            bn = nn.BatchNorm2d
+        else:
+            conv_nd = nn.Conv1d
+            max_pool_layer = nn.MaxPool1d(kernel_size=(2))
+            bn = nn.BatchNorm1d
+
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                         kernel_size=1, stride=1, padding=0)
+
+        if bn_layer:
+            self.W = nn.Sequential(
+                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                        kernel_size=1, stride=1, padding=0),
+                bn(self.in_channels)
+            )
+            nn.init.constant_(self.W[1].weight, 0)
+            nn.init.constant_(self.W[1].bias, 0)
+        else:
+            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                             kernel_size=1, stride=1, padding=0)
+            nn.init.constant_(self.W.weight, 0)
+            nn.init.constant_(self.W.bias, 0)
+
+        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+
+        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+        if sub_sample:
+            self.g = nn.Sequential(self.g, max_pool_layer)
+            self.phi = nn.Sequential(self.phi, max_pool_layer)
+
+    def forward(self, x, return_nl_map=False):
+        """
+        :param x: (b, c, t, h, w)
+        :param return_nl_map: if True return z, nl_map, else only return z.
+        :return:
+        """
+
+        batch_size = x.size(0)
+
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+        theta_x = theta_x.permute(0, 2, 1)
+        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+
+        f = torch.matmul(theta_x, phi_x)
+        N = f.size(-1)
+        f_div_C = f / N
+
+        y = torch.matmul(f_div_C, g_x)
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
+        W_y = self.W(y)
+        z = W_y + x
+
+        if return_nl_map:
+            return z, f_div_C
+        return z
+
+
+class NONLocalBlock1D(_NonLocalBlockND):
+    def __init__(self, in_channels, inter_channels=None, sub_sample=True, bn_layer=True):
+        super(NONLocalBlock1D, self).__init__(in_channels,
+                                              inter_channels=inter_channels,
+                                              dimension=1, sub_sample=sub_sample,
+                                              bn_layer=bn_layer)
+
+
+class Aggregate(nn.Module):
+    def __init__(self, len_feature):
+        super(Aggregate, self).__init__()
+        bn = nn.BatchNorm1d
+        self.len_feature = len_feature
+        self.conv_1 = nn.Sequential(
+            nn.Conv1d(in_channels=len_feature, out_channels=512, kernel_size=3,
+                      stride=1, dilation=1, padding=1),
+            nn.ReLU(),
+            bn(512)
+            # nn.dropout(0.7)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv1d(in_channels=len_feature, out_channels=512, kernel_size=3,
+                      stride=1, dilation=2, padding=2),
+            nn.ReLU(),
+            bn(512)
+            # nn.dropout(0.7)
+        )
+        self.conv_3 = nn.Sequential(
+            nn.Conv1d(in_channels=len_feature, out_channels=512, kernel_size=3,
+                      stride=1, dilation=4, padding=4),
+            nn.ReLU(),
+            bn(512)
+            # nn.dropout(0.7),
+        )
+        self.conv_4 = nn.Sequential(
+            nn.Conv1d(in_channels=2048, out_channels=512, kernel_size=1,
+                      stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            # nn.dropout(0.7),
+        )
+        self.conv_5 = nn.Sequential(
+            nn.Conv1d(in_channels=2048, out_channels=2048, kernel_size=3,
+                      stride=1, padding=1, bias=False),  # should we keep the bias?
+            nn.ReLU(),
+            nn.BatchNorm1d(2048),
+            # nn.dropout(0.7)
+        )
+
+        # self.non_local = NONLocalBlock1D(512, sub_sample=False, bn_layer=True)
+        self.non_local = MultiHeaded_NONLocalBlock1D(512, sub_sample=False, bn_layer=True, n_heads=2)
+
+        # self.drop_out = nn.Dropout(0.5)
+
+    def forward(self, x):  # (bs,32,2048)
+            # x: (B, T, F)
+            out = x.permute(0, 2, 1)
+            residual = out
+
+            out1 = self.conv_1(out)
+            out2 = self.conv_2(out)
+
+            out3 = self.conv_3(out)
+            out_d = torch.cat((out1, out2, out3), dim=1)
+            out = self.conv_4(out)
+            out = self.non_local(out)  # (bs,512,1)-> (bs,512,1)
+            out = torch.cat((out_d, out), dim=1)
+            out = self.conv_5(out)   # fuse all the features together
+            out = out + residual
+            out = out.permute(0, 2, 1)
+            # out: (B, T, F)
+
+            # dropout
+            # out = self.drop_out(out)
+
+            # out = F.avg_pool1d(out, 8)
+            # out = out.reshape(-1, self.len_feature)
+            out = out.view(-1, self.len_feature)  # （bs,1,2048）
+
+            return out
+
+
+# 两层全连接层
+class Linear_Module(nn.Module):
+    def __init__(self, arg):
+        super().__init__()
+        self.feat_dim = arg.feat_dim  # 2048
+        self.low_dim = arg.low_dim  # 256
+        self.num_cluster = arg.num_cluster  # 10
+        self.relu = nn.ReLU(inplace=True)
+        self.embeddings_layer = nn.Linear(self.feat_dim, self.low_dim)
+        self.cluster_layer = nn.Sequential(
+            # nn.Linear(self.low_dim, self.low_dim),
+            # nn.ReLU(),
+            nn.Linear(self.low_dim, self.num_cluster),
+        )
+
+    def forward(self, x):
+        embeddings = self.embeddings_layer(self.relu(x))
+        clusters = self.cluster_layer(self.relu(embeddings))
+        return embeddings, clusters
+
+
+# 判别器
+class Discriminator(nn.Module):
+    def __init__(self, fea_in, memory_size):
+        super(Discriminator, self).__init__()
+        self.fea_in = fea_in
+        self.memory_size = memory_size
+        self.model = nn.Sequential(
+            nn.Linear(self.fea_in, self.memory_size),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(self.memory_size, 1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        return self.model(x)
